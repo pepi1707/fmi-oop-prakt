161faa80829ba243ecbbc91eb42a9d4476bfc52c
Peter Angelov <30953569+pepi1707@users.noreply.github.com>
2019-03-22 14:01:39 +0200

    Add files via upload

---
+++ week04/vector.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template <typename T>
+class Vector
+{
+    T *elements;
+    size_t numElements;
+    size_t capacity;
+
+    void resize()
+    {
+        capacity *= 2;
+        T *cur = new T[capacity];
+        for(int i = 0; i < numElements; i++)
+        {
+            cur[i] = elements[i];
+        }
+        delete[] elements;
+        elements = cur;
+    }
+
+public:
+    Vector()
+    {
+        elements = new T[1];
+        numElements = 0;
+        capacity = 1;
+    }
+
+    ~Vector()
+    {
+        delete[] elements;
+    }
+
+    Vector(const Vector& toCopy)
+    {
+        numElements = toCopy.numElements;
+        capacity = toCopy.capacity;
+        elements = new T[capacity];
+        for(int i = 0; i < numElements; i++)
+            elements[i] = toCopy.elements[i];
+    }
+
+    void push(const T& toPush)
+    {
+        if(numElements == capacity)
+        {
+            resize();
+        }
+        elements[numElements] = toPush;
+        numElements ++;
+    }
+
+    size_t size()
+    {
+        return numElements;
+    }
+
+    T& operator [] (size_t x)
+    {
+        assert(x < numElements && x >= 0);
+        return elements[x];
+    }
+};
+
+
+int main()
+{
+    Vector<int> v;
+    v.push(2);
+    v.push(3);
+    cout << v[0] << endl;
+	return 0;
+}
